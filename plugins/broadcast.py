e91fc795f5656df7ea524ab7322d8fbd2f1df3cb
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-04-02 01:11:12 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,32 +1,26 @@
-from pyrogram import Client, filters
-import datetime
-import time
-from database.users_chats_db import db
-from info import ADMINS
-from utils import broadcast_messages
 import asyncio
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
 
-@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-async def broadcast_message(bot, message):
-    b_msg = message.reply_to_message
-    if not b_msg:
-        await message.reply_text("Please reply to a message to broadcast.")
-        return
-    users = await db.get_all_users()
-    total_users = await db.total_users_count()
-    start_time = time.time()
-    sent = 0
-    failed = 0
-    for user in users:
-        user_id = int(user['id'])
-        try:
-            await b_msg.copy(user_id)
-            sent += 1
-        except Exception as e:
-            print(f"Failed to send message to {user_id}: {str(e)}")
-            failed += 1
-        if sent % 20 == 0:
-            await message.reply_text(f"Sent to {sent}/{total_users} users.")
-        await asyncio.sleep(2)
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await message.reply_text(f"Broadcast completed in {time_taken}. Sent to {sent}/{total_users} users. Failed for {failed} users.")
+@Client.on_message(filters.command('broadcast') & filters.private & filters.user("your_username_here"))
+async def broadcast(client, message):
+    try:
+        sent = 0
+        failed = 0
+        chat_list = await client.get_dialogs()
+        total = len(chat_list)
+        for dialog in chat_list:
+            try:
+                if dialog.chat.type == "private":
+                    await client.send_message(dialog.chat.id, message.text)
+                    sent += 1
+                    await asyncio.sleep(1)
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+            except Exception:
+                failed += 1
+        await message.reply_text(f"Message sent to {sent} chats. {failed} failed.")
+    except Exception as e:
+        print(e)
+        await message.reply_text("An error occurred!")
+
