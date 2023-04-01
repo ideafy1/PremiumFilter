1df6d62ae5c41e0d0b7813777c0ac8ce09ea47b3
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-04-02 01:20:28 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,26 +1,41 @@
-import asyncio
 from pyrogram import Client, filters
-from pyrogram.errors import FloodWait
+import datetime
+import time
+from database.users_chats_db import db
+from info import ADMINS
+from utils import broadcast_messages
+import asyncio
+        
+@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
+# https://t.me/GetTGLink/4178
+async def verupikkals(bot, message):
+    users = await db.get_all_users()
+    b_msg = message.reply_to_message
+    sts = await message.reply_text(
+        text='Bʀᴏᴀᴅᴄᴀsᴛɪɴɢ Yᴏᴜʀ Mᴇssᴀɢᴇ Tᴏ Yᴏᴜʀ Bᴏᴛ Usᴇʀs 😗 Sᴜᴩᴩᴏʀᴛ ﹝ MOVIES HUB ALPHA ﹞...'
+    )
+    start_time = time.time()
+    total_users = await db.total_users_count()
+    done = 0
+    blocked = 0
+    deleted = 0
+    failed =0
 
-@Client.on_message(filters.command('broadcast') & filters.private & filters.user("your_username_here"))
-async def broadcast(client, message):
-    try:
-        sent = 0
-        failed = 0
-        chat_list = await client.get_dialogs()
-        total = len(chat_list)
-        for dialog in chat_list:
-            try:
-                if dialog.chat.type == "private":
-                    await client.send_message(dialog.chat.id, message.text)
-                    sent += 1
-                    await asyncio.sleep(1)
-            except FloodWait as e:
-                await asyncio.sleep(e.x)
-            except Exception:
+    success = 0
+    async for user in users:
+        pti, sh = await broadcast_messages(int(user['id']), b_msg)
+        if pti:
+            success += 1
+        elif pti == False:
+            if sh == "Blocked":
+                blocked+=1
+            elif sh == "Deleted":
+                deleted += 1
+            elif sh == "Error":
                 failed += 1
-        await message.reply_text(f"Message sent to {sent} chats. {failed} failed.")
-    except Exception as e:
-        print(e)
-        await message.reply_text("An error occurred!")
-
+        done += 1
+        await asyncio.sleep(2)
+        if not done % 20:
+            await sts.edit(f"Bʀᴏᴀᴅᴄᴀsᴛɪɴɢ Nᴏᴡ:\n\nTᴏᴛᴀʟ Usᴇʀs ﹦ {total_users}\nCᴏᴍᴩʟᴇᴛᴇᴅ ﹦ {done} / {total_users}\nSᴜᴄᴄᴇss ﹦ {success}\nBʟᴏᴄᴋᴇᴅ ﹦ {blocked}\nDᴇʟᴇᴛᴇᴅ ﹦ {deleted}")    
+    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+    await sts.edit(f"Bʀᴏᴀᴅᴄᴀsᴛ Cᴏᴍᴩʟᴇᴛᴇᴅ ﹦\nCᴏᴍᴩʟᴇᴛᴇᴅ Iɴ ﹦ {time_taken} Sᴇᴄᴏɴᴅs.\n\nTᴏᴛᴀʟ Usᴇʀs ﹦ {total_users}\nCᴏᴍᴩʟᴇᴛᴇᴅ {done} / {total_users}\nSᴜᴄᴇss ﹦ {success}\nBʟᴏᴄᴋᴇᴅ ﹦ {blocked}\nDeleted: {deleted}")
