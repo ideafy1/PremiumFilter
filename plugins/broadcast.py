483b23b199372948a8d20c295e5cb23f029605d8
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-04-02 09:19:23 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -12,7 +12,7 @@ async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
     sts = await message.reply_text(
-        text='Bʀᴏᴀᴅᴄᴀsᴛɪɴɢ Yᴏᴜʀ Mᴇssᴀɢᴇ Tᴏ Yᴏᴜʀ Bᴏᴛ Usᴇʀs 😗 Sᴜᴩᴩᴏʀᴛ ﹝ MOVIES HUB ALPHA ﹞...'
+        text='Bʀᴏᴀᴅᴄᴀsᴛɪɴɢ Yᴏᴜʀ Mᴇssᴀɢᴇ Tᴏ Yᴏᴜʀ Bᴏᴛ Usᴇʀs 😗 Sᴜᴩᴩᴏʀᴛ ﹝ ᴛᴀᴍɪʟᴀɴ_ʙᴏᴛsᴢ ﹞...'
     )
     start_time = time.time()
     total_users = await db.total_users_count()
