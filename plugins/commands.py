3661893e19a1a85f5fa023899ede12caa5029212
Aditya Singh <118215356+ideafy1@users.noreply.github.com>
2023-07-30 09:14:03 +0530

    commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -2,6 +2,7 @@ import os
 import logging
 import random
 import asyncio
+from io import open
 from Script import script
 from pyrogram import Client, filters, enums
 from pyrogram.errors import ChatAdminRequired, FloodWait
