0aaed8450fb4c5d60670a30f2b002ae8d68494b8
Aditya Singh <118215356+ideafy1@users.noreply.github.com>
2023-08-01 15:33:33 +0530

    Script.py

--- Script.py
+++ Script.py
@@ -21,15 +21,10 @@ class script(object):
 
   ABOUT_TXT = """YeDekho Search Engine 🔍\n\n
 🧠 BRAIN : <a href=https://t.me/ideafy>telegram</a>
-\n\n
 🔤 LANGUAGE : not english 😏
-\n\n
 💀 FRAMEWORK : 🐍3
-\n\n
 🏡 HOME : <a href=https://www.render.com/>RENDER</a>
-\n\n
 🧐 SUPPORT: <a href=https://t.me/yedekho_faq>TOUCH ME</a>
-\n\n
 🐣 SOURCE : <a href=https://t.me/about_yedekho/2>BEAT ME</a>"""
 
   SOURCES_TXT = """Marvelous creation of yedekho devs ✅⚡ @yedekho
